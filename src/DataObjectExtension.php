6bca0632ab95a4e10009e6c0dff18d9aeaa5ee60
Morven Lewis-Everley <morven@i-lateral.com>
2022-01-16 16:54:02 +0000

    Fix merge issues

--- src/DataObjectExtension.php
+++ src/DataObjectExtension.php
@@ -6,25 +6,19 @@ use SilverStripe\ORM\DataExtension;
 
 class DataObjectExtension extends DataExtension
 {
-<<<<<<< HEAD
-=======
     /**
      * Return an array of default filters to load for this object
      *
      * @return array
      */
->>>>>>> 51f34a4d5b526cbf202d8d500c8d89fa5b006c99
     public function getDefaultSearchFilter(): array
     {
         return (array)$this->getOwner()->config()->get("default_search_filter");
     }
 
-<<<<<<< HEAD
-=======
     /**
      * Get a custom search context for model admin plus
      */
->>>>>>> 51f34a4d5b526cbf202d8d500c8d89fa5b006c99
     public function getModelAdminSearchContext()
     {
         return SearchContext::create(
